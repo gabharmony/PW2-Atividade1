928de474c8658ad87b5acf44936b986b772ddbfa
Gabriel Harmony <gabrielharmony@hotmail.com>
2020-03-11 22:23:40 -0300

    Codificação de método possoIrBanco() com outro Modo de fazer

--- src/ExercicioDois.java
+++ src/ExercicioDois.java
@@ -3,15 +3,17 @@ public class ExercicioDois {
 
 	public static void main(String[] args) {
 
-		boolean diaHoraValido = possoIrBanco("Quarta", 18);
+		boolean diaHoraValido = possoIrBanco("Quarta", 17);
 
 		System.out.println("Posso ir ao Banco: " + diaHoraValido);
 
 	}
 
 	public static boolean possoIrBanco(String dia, int hora) {
+		
+		return ExercicioUm.ehDiaSemana(dia) && hora >= 10 && hora <= 16;
 
-		return ! (hora < 10 || hora > 16 || dia.equalsIgnoreCase("domingo") || dia.equalsIgnoreCase("sábado"));
+		//return ! (dia.equalsIgnoreCase("domingo") || dia.equalsIgnoreCase("sábado") || hora <= 10 || hora >= 16);
 
 	}
 
